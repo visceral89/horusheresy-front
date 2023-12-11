09f52beaaf58579dca53d971b3bed83dc049c4f9
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-11 15:07:10 +0100

    moved image to navbar

--- app/page.jsx
+++ app/page.jsx
@@ -7,10 +7,6 @@ export default function Home() {
 	return (
 		<main className={styles.main}>
 			<div className={styles.header}>
-				<div className={styles.headerImage}>
-					
-				</div>
-
 				<div className={styles.headerTitle}>
 					<h1 className={styles.heading1}>Welcome to the Horus Heresy API</h1>
 					<p className={styles.subHeading1}>
