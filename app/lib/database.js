3f1be6ca13904c6e23e0133e01c9f924151273ee
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-13 09:41:21 +0100

    Update database.js

--- app/lib/database.js
+++ app/lib/database.js
@@ -1,7 +1,7 @@
-import {Pool} from pg
+import { Pool } from "pg";
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+	connectionString: process.env.POSTGRES_URL,
 });
 
 export default pool;
