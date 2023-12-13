3aa071cf6d63c6e0845a914c87e7716c0f3900dc
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-13 09:45:02 +0100

    database ssl

--- app/lib/database.js
+++ app/lib/database.js
@@ -2,6 +2,9 @@ import { Pool } from "pg";
 
 const pool = new Pool({
 	connectionString: process.env.POSTGRES_URL,
+	ssl: {
+		rejectUnauthorized: false, // For self-signed certificates
+	},
 });
 
 export default pool;
