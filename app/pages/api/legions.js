b259827928968a0c6703ab33961aae194e766c3e
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-13 09:09:23 +0100

    moved api endpoints into project

--- app/pages/api/legions.js
+++ app/pages/api/legions.js
@@ -2,6 +2,9 @@ import pool from "../../lib/database";
 
 export default async (req, res) => {
 	try {
-        const {rows:legions} = await pool.query
-	} catch (err) {}
+		const { rows: legions } = await pool.query("SELECT * FROM legions");
+		res.status(200).json(legions);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
 };
