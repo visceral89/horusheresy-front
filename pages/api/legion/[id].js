c233f9fa5929384687d555ac6ac9f8681ed99265
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-13 11:24:24 +0100

    Update [id].js

--- pages/api/legion/[id].js
+++ pages/api/legion/[id].js
@@ -1,17 +1,19 @@
+// pages/api/legion/[id].js
 import pool from "../../../app/lib/database";
 
 export default async (req, res) => {
-	const legionId = parseInt(req.params.id);
+	const legionId = parseInt(req.query.id); // Use req.query instead of req.params
 
 	try {
-		const query = "SELECT * FROM legions WHERE id=$1";
+		const query = "SELECT * FROM legions WHERE id = $1";
 		const { rows: legion } = await pool.query(query, [legionId]);
+
 		if (legion.length) {
-			req.status(200).json(legion[0]);
+			res.status(200).json(legion[0]); // Use res instead of req for sending response
 		} else {
 			res.status(404).send("Legion not found, try 1-20");
 		}
 	} catch (err) {
-		res.status(500).send("YOu need moore deditated wam to the sorvoor");
+		res.status(500).send("You need more dedicated RAM to the server");
 	}
 };
