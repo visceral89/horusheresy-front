2e767cc88592c8601e53879c1419dbc949c8927c
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-13 11:40:06 +0100

    i dont need IF statement

--- pages/api/loyalists.js
+++ pages/api/loyalists.js
@@ -4,12 +4,7 @@ export default async (req, res) => {
 	try {
 		const query = "SELECT * FROM legions WHERE traitor = false";
 		const { rows: loyalistLegions } = await pool.query(query);
-
-		if (loyalistLegions.length) {
-			res.status(200).json(loyalistLegions);
-		} else {
-			res.status(404).send("Legion not found, try 1-20");
-		}
+		res.status(200).json(loyalistLegions);
 	} catch (err) {
 		res.status(500).send("You need more dedicated RAM to the server");
 	}
--- pages/api/traitors.js
+++ pages/api/traitors.js
@@ -4,12 +4,7 @@ export default async (req, res) => {
 	try {
 		const query = "SELECT * FROM legions WHERE traitor = true";
 		const { rows: traitorLegions } = await pool.query(query);
-
-		if (traitorLegions.length) {
-			res.status(200).json(traitorLegions);
-		} else {
-			res.status(404).send("Legion not found, try 1-20");
-		}
+		res.status(200).json(traitorLegions);
 	} catch (err) {
 		res.status(500).send("You need more dedicated RAM to the server");
 	}
