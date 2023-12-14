9cb0e9e79271111b31c12b1520fceb7cf9bd411e
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-14 21:55:07 +0100

    Update page.jsx

--- app/legions/page.jsx
+++ app/legions/page.jsx
@@ -1,18 +1,7 @@
 import Link from "next/link";
 import Card from "../components/Card";
 
-async function getLegions() {
-	const res = await fetch("https://horus-heresy-next.vercel.app/api/legions");
-
-	if (!res.ok) {
-		throw new Error("Failed to fetch data...");
-	}
-	return res.json;
-}
-
 export default async function Legions() {
-	const legions = await getLegions();
-
 	return (
 		<>
 			<h1>Legions Page</h1>
