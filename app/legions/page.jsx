f7b2aecafbb690ab50dc24ef4b284e67a59a81eb
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-19 17:00:30 +0100

    excluding primarch image

--- app/legions/page.jsx
+++ app/legions/page.jsx
@@ -24,12 +24,7 @@ export default async function Legions() {
 			<section className={styles.cardSection}>
 				{legions.map((legion) => (
 					<Link key={legion.id} href={`/legions/${legion.id}`}>
-						<Card
-							key={legion.id}
-							id={legion.id}
-							name={legion.name}
-							primarch_image={legion.primarch_image_url}
-						/>
+						<Card key={legion.id} id={legion.id} name={legion.name} />
 					</Link>
 				))}
 			</section>
