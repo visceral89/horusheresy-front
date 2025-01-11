4d50b07a7b2e159efb8cc985cc60d9a8a8246263
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-01-11 19:09:35 +0100

    Middleware and Updating fetching

--- app/lib/getLegion.jsx
+++ app/lib/getLegion.jsx
@@ -1,7 +1,11 @@
+import { headers } from "@/next.config";
+
 export default async function getLegion(id) {
-	const response = await fetch(
-		`https://horus-heresy-next.vercel.app/api/legion/${id}`
-	);
+	const response = await fetch(`https://horus-heresy-next.vercel.app/api/legion/${id}`, {
+		headers: {
+			"x-api-key": process.env.API_KEY,
+		},
+	});
 
 	if (!response.ok) {
 		throw new Error("Couldnt fetch legion");
--- app/lib/getLegions.jsx
+++ app/lib/getLegions.jsx
@@ -1,7 +1,7 @@
 export default async function getLegions() {
-	const response = await fetch(
-		"https://horus-heresy-next.vercel.app/api/legions"
-	);
+	const response = await fetch("https://horus-heresy-next.vercel.app/api/legions", {
+		headers: { "x-api-key": process.env.API_KEY },
+	});
 
 	if (!response.ok) {
 		throw new Error("Couldnt fetch data, you suck.");
